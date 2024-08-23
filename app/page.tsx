284c85e941a6a76f8894c31c9a20bf4f3ef22ee7
Aldacelio <aldacelio@alu.ufc.br>
2024-08-23 15:07:00 -0300

    feat: add SearchBar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import { Hero } from "./_components";
+import CustomFilter from "./_components/CustomFilter";
+import SearchBar from "./_components/SearchBar";
 
 
 
@@ -6,6 +8,21 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Catálogo de Carros</h1>
+          <p>Explore os carros que você pode gostar</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+
+          {/* <div className="home__filter-container">
+            <CustomFilter title="Combustível" />
+            <CustomFilter title="Ano" />
+          </div> */}
+        </div>
+      </div>
     </main>
   );
 }
